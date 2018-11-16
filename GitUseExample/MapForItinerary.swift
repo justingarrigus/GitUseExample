5f93b24471273a25acedec6a72ad0796bd5c68c1
Garrigus, Justin <122341@s020rmc148-16.local>
2018-11-16 11:12:28 -0600

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Garrigus, Justin on 11/16/18.
+//  Copyright © 2018 Garrigus, Justin. All rights reserved.
+//
+
+import Foundation
